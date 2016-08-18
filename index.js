f00648da86648b6d0b878deb60da5ac75c84e553
Sebastian Qu <sebastianqu@gmail.com>
2016-08-18 15:42:36 +0800

    chore(commander): 添加commander包，并实现入口参数
    依赖commander包，来实现node index.js的入口参数.

--- index.js
+++ index.js
@@ -1,10 +1,37 @@
+let fs = require('fs');
+let program = require('commander');
+
+
+// Set Application's useage
+program.version('0.1.0')
+  .usage('-c <configuration file>')
+  .option('-c, --config <file ...>', 'Sets the path to a configuration file on disk')
+
+program.on('--help', function(){
+  console.log('  Examples:');
+  console.log('');
+  console.log('    $ node index.js');
+  console.log('    $ node idnex.js -c config.json');
+  console.log('    $ node idnex.js --config config.json');
+  console.log('');
+});
+
+program.parse(process.argv);
+
+// Get custom configuration file path from argument
+let customConfigFile = program.config
+
 // Load default Configuration
 let defaultConfig = require('./lib/config/');
 
-// TODO Load custom cofiguration from file system.
+// Load custom cofiguration from file system.
+//let customConfig = JSON.parse(fs.readFileSync(customConfigFile));
 
 // Merge custom configuration and default configuration
-let config = defaultConfig;
+//let config = defaultConfig;
+//if (customConfigFile !== "") {
+//  config = Object.assign(config
+//}
 
 // startup gateway
 let gateway =  require('./lib/gateway/')(config);

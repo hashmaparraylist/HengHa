c4724485f8539d3ddbf1ee124f203b353f9f0954
Sebastian Qu <sebastianqu@gmail.com>
2016-08-24 17:37:07 +0800

    feat(gateway): add proxy logic

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ let path = require('path');
 let program = require('commander');
 let _ = require('lodash');
 
+// Load default Configuration
+let defaultConfig = require('./lib/config/');
+
 // Set Application's useage
 program.version('0.1.0')
   .usage('-c <configuration file>')
@@ -24,21 +27,17 @@ program.parse(process.argv);
 
 // Get custom configuration file path from argument
 let customConfigFile = program.config || '';
-console.log(customConfigFile);
-
-// Load default Configuration
-let defaultConfig = require('./lib/config/');
 
 // Merge custom configuration and default configuration
 let config = defaultConfig;
 if (customConfigFile.length > 0) {
   // Load custom cofiguration from file system.
-  let customConfig = JSON.parse(fs.readFileSync(customConfigFile));
+  let customConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, customConfigFile)));
   config = _.assignIn({}, defaultConfig, customConfig);
 }
 
 // Resolve relative path to absolute path
-config.data.file = path.resolve(config.data.file);
+config.data.file = path.resolve(__dirname, config.data.file);
 // Set config into Global namespace
 global.config = config;
 

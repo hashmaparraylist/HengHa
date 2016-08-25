58b9b4ec0b109ba7fe41e9826d794845581c62da
Sebastian Qu <sebastianqu@gmail.com>
2016-08-25 14:18:19 +0800

    refactor(*): 重构加载配置文件模块
    将读取自定义配置文件，整合默认和自订配置文件功能等全部移入配置模块

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ let program = require('commander');
 let _ = require('lodash');
 
 // Load default Configuration
-let defaultConfig = require('./lib/config/');
+let configLoader = require('./lib/config/');
 
 // Set Application's useage
 program.version('0.1.0')
@@ -28,16 +28,9 @@ program.parse(process.argv);
 // Get custom configuration file path from argument
 let customConfigFile = program.config || '';
 
-// Merge custom configuration and default configuration
-let config = defaultConfig;
-if (customConfigFile.length > 0) {
-  // Load custom cofiguration from file system.
-  let customConfig = JSON.parse(fs.readFileSync(path.resolve(__dirname, customConfigFile)));
-  config = _.assignIn({}, defaultConfig, customConfig);
-}
+// Get application config
+let config = configLoader.getConfig(customConfigFile);
 
-// Resolve relative path to absolute path
-config.data.file = path.resolve(__dirname, config.data.file);
 // Set config into Global namespace
 global.config = config;
 
